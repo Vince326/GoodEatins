f132c66611fa2fc2a5b30b22fdf3403b17cbcbec
Vincent Hunter <jhearst29@gmail.com>
2020-06-14 09:43:02 -0400

    Initial Commit

---
+++ GoodEatins2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodEatins2
+//
+//  Created by Vincent Hunter on 6/14/20.
+//  Copyright © 2020 Vincent Hunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
